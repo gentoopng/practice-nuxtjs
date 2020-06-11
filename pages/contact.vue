c487f8d705e0b4abcf661af529dca47d28d5a5c4
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-06-11 12:43:02 +0900

    Create "about" and "contact" pages

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="hero is-info is-bold">
+        <div class="hero-body">
+            <h1 class="title is-size-2">
+                Contact
+            </h1>
+            <h2 class="subtitle is-size-4">
+                お問い合わせ
+            </h2>
+        </div>
+    </section>
+</template>
